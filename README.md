d4cec8ab810fda8bbd9dc3f77e1c901da83a0963
Billy Fung <ckbfung@gmail.com>
2017-01-30 23:35:52 -0500

    add first version

---
+++ README.md
@@ -0,0 +1,221 @@
+# Data flow task
+
+## Installation
+
+```
+$ npm install data-flow-task
+```
+
+## Data Sources
+
+Define a name in KEY for Data-Flow-Task to reference.
+
+DB can be 'MSSQL' or 'Oracle'.
+
+ConnectionString is connection string to connect to DB.
+
+eg,
+    'MS SQL Source': {
+        DB: 'MSSQL',
+        ConnectionString: "Data Source=Hostname\\DbInstance;Initial Catalog=DbName;Integrated Security=True"
+    }
+
+## Tasks
+
+There are four Tasks supported.
+
+* Run SQL
+* Copy DB Table
+* Insert DB data
+* Insert CSV data
+
+### Run SQL
+
+Define a TaskType: 'Run SQL'.
+
+DataSource is defined in 'Data Sources' configuration.
+
+Queries is to define queries to be executed. Queries are sequential execution in order.
+
+eg,
+    TaskType: 'Run SQL',
+    DbSource: 'MS SQL Source',
+    Queries: [
+        'DELETE FROM Table',
+        "INSERT INTO Table(ColumnA, ColumnB) values(number, 'VarChar')"
+    ]
+
+### Copy DB Table
+
+Define a TaskType: 'Copy DB Table'.
+
+DbSource and DbDestination are defined in 'Data Sources' configuration.
+
+TableName is the table to copy from DbSource DB to DbDestination DB.
+
+eg,
+    TaskType: 'Copy DB Table',
+    DbSource: 'MS SQL Source',
+    DbDestination: 'MS SQL Dest',
+    TableName: 'Table',
+
+### Insert DB data
+
+Define a TaskType: 'Insert DB data'.
+
+DbSource and DbDestination Names are defined in 'Data Sources' configuration.
+
+DbSouce Query is the source query to retrive data from DbSource.
+
+Transforms are function names to inject to Data-Flow-Task. After data is obtained from DbSource Query, Data-Flow-Task executs the transform functions in sequential.
+
+DbDestination Queries is to define queries to be executed. Queries are sequential execution in order.
+
+eg,
+    TaskType: 'Insert DB data',
+    DbSource: {
+        Name: 'MS SQL Source',
+        Query: 'Select COL1, COL2 from Table where id = ${Param.ID}'
+    },
+    Transforms: ['Transform1', 'Transform2', 'Transform3'],
+    DbDestination: {
+        Name: 'MS SQL Dest',
+        Queries: [
+            "Insert Into DestTable (DestCol1, DestCol2) Values(${Row.COL1}, '${Row.COL2}')"
+        ]
+    }
+
+### Insert CSV data
+
+Define a TaskType: 'Insert CSV data'.
+
+CsvSource specifies File name, Skip Headers, and Delimiter.
+
+Transforms are function names to inject to Data-Flow-Task. After data is obtained from CSV file, Data-Flow-Task executs the transform functions in sequential.
+
+DbDestination Names is defined in 'Data Sources' configuration.
+
+DbDestination Queries is to define queries to be executed. Queries are sequential execution in order.
+
+eg,
+    TaskType: 'Insert CSV data',
+    CsvSource: {
+        File: './test.csv',
+        SkipHeader: true,
+        Delimiter: ','
+    },
+    Transforms: ['TransformCSV'],
+    DbDestination: {
+        Name: 'MS SQL Dest',
+        Queries: [
+            "Insert Into fok (a,b) Values(${Row[0]},'${Row[1]}')",
+            "Insert Into fok (b) Values('${Row[2]}')"
+        ]
+    }
+
+## Usage
+
+```js
+var DataFlowTask = require('data-flow-task')
+
+// Define Data Source
+var dataSources = {
+    'MS SQL Source': {
+        DB: 'MSSQL',
+        ConnectionString: "Data Source=Hostname\\DbInstance;Initial Catalog=DbName;Integrated Security=True"
+    },
+    'MS SQL Dest': {
+        DB: 'MSSQL',
+        ConnectionString: "Data Source=Hostname\\DbInstance;Initial Catalog=DbName;Integrated Security=True"
+    }
+}
+
+var dataFlow = {
+    Cleanup: {
+		TaskType: 'Run SQL',
+		DbSource: 'MS SQL Source',
+		Queries: [
+            'DELETE FROM Table',
+            'DELETE FROM ${Param.TableName}'
+        ],
+	},
+	CopyTable: {
+		TaskType: 'Copy DB Table',
+		DbSource: 'MS SQL Source',
+		DbDestination: 'MS SQL Dest',
+		TableName: '${Param.TableName}',
+	},
+    // InsertData is an Array. Each task in Array are executed asynchronously.
+	InsertData: [
+		{
+			TaskType: 'Insert DB data',
+			DbSource: {
+				Name: 'MS SQL Source',
+				Query: 'Select COL1, COL2 from Table Where ID = ${Param.ID}'
+			},
+			Transforms: ['Transform1', 'Transform2', 'Transform3'],
+			DbDestination: {
+				Name: 'MS SQL Dest',
+				Queries: [
+                    "Insert Into DestTableA (DestCol1, DestCol2) Values(${Row.COL1}, '${Row.COL2}')",
+                    "Update DestTableB Set DestCol1=${Row.COL1}, DestCol2='${Row.COL2}' Where ID = ${Param.ID}"
+                ]
+			}
+		},
+		{
+			TaskType: 'Insert CSV data',
+			CsvSource: {
+				File: './CSVFile.csv',
+                SkipHeader: true,
+				Delimiter: ','
+			},
+			Transforms: ['TransformCSV'],
+			DbDestination: {
+				Name: 'MS SQL Dest',
+				Queries: [
+                    "Insert Into DestTableA (DestCol1, DestCol2) Values(${Row.COL1}, '${Row.COL2}')",
+                    "Update DestTableB Set DestCol1=${Row.COL1}, DestCol2='${Row.COL2}' Where ID = ${Param.ID}"
+                ]
+            }
+		}
+	]
+}
+
+function Transform1(row, context, callback) {
+    console.log('Call Transform1')
+    callback()
+}
+
+function Transform2(row, context, callback) {
+    console.log('Call Transform2')
+    callback()
+}
+
+function Transform3(row, context, callback) {
+    console.log('Call Transform3')
+    callback()
+}
+
+function TransformCSV(row, context, callback) {
+    console.log('Call TransformCSV')
+    callback()
+}
+
+var dataFlowTask = DataFlowTask(dataSources, dataFlow)
+dataFlowTask.on('Message', function(msg) {
+    console.log('Message:', msg)
+})
+
+dataFlowTask.on('Query', function(msg) {
+    console.log('Query:', msg)
+})
+
+dataFlowTask.Start(
+    { ID: 10, TableName:'TestTable' },
+    [Transform1, Transform2, Transform3, TransformCSV],
+    function(err) {
+        if (err) {
+            console.log('Error:', err)
+        }
+})
+```
---
+++ index.js
@@ -0,0 +1,473 @@
+'use strict'
+
+var util = require('util')
+var fs = require('fs')
+var parse = require('csv-parse')
+var eventEmitter = require('events').EventEmitter
+var dbConnection = require('./dbConnection')
+
+function DataFlowTask(dataSource, dataFlow) {
+    if ((this instanceof DataFlowTask) == false) {
+        return new DataFlowTask(dataSource, dataFlow)
+    }
+
+    this.DataSource = dataSource
+    this.DataFlow = dataFlow
+
+    eventEmitter.call(this)
+}
+
+util.inherits(DataFlowTask, eventEmitter)
+
+var getCallback = function(callback) {
+    if (callback != null) {
+        return callback
+    }
+    return function(err) {
+        if (err) {
+            throw err
+        }
+    }
+}
+
+function getConnection(dataSouceName, dataSource, callback) {
+    if (dataSouceName == null) {
+        callback("Empty DataSource")
+    } else if (dataSouceName in dataSource) {
+        var dsConfig = dataSource[dataSouceName]
+        if (dsConfig.DB == null) {
+            callback(`${dataSourceName}: Miss DB.`)
+        } else if (dsConfig.ConnectionString == null) {
+            callback(`${dataSourceName}: Miss ConnectionString.`)
+        } else {
+            callback(null, dbConnection(dsConfig.DB, dsConfig.ConnectionString))
+        }
+    } else {
+        callback("Invalid DataSource")
+    }
+}
+
+function runSQL(name, task, config, emitter, callback) {
+    emitter.emit('Message', `Start ${task.TaskType} ${name}.`)
+
+    if (task.Queries != null && util.isArray(task.Queries)) {
+        emitter.emit('Message', task.DbSource)
+        getConnection(task.DbSource, config.DataSource, function(err, conn) {
+            if (err) {
+                callback(err)
+            } else if (conn == null) {
+                callback(`Fail to connection to ${task.DbSource}.`)
+            } else {
+                var Param = config.Param
+                var executeQuery = function(idx) {
+                    if (idx >= task.Queries.length) {
+                        callback(null)
+                    } else {
+                        var query = eval("`" + task.Queries[idx] + "`")
+                        emitter.emit('Query', `${task.DbSource}: Execute: ${query}`)
+
+                        conn.execute(query, function(error, result) {
+                            if (error) {
+                                callback(error)
+                            } else {
+                                executeQuery(++idx)
+                            }
+                        })
+                    }
+                }
+                executeQuery(0)
+            }
+        })
+    } else {
+        callback(null)
+    }
+}
+
+function copyDbTable(name, task, config, emitter, callback) {
+    emitter.emit('Message', `Start ${task.TaskType} ${name}.`)
+
+    if (task.TableName != null) {
+        emitter.emit('Message', task.DbSource)
+        getConnection(task.DbSource, config.DataSource, function(err, srcConn) {
+            if (err) {
+                callback(err)
+            } else if (srcConn == null) {
+                callback(`Fail to connection to ${task.DbSource}.`)
+            } else {
+                emitter.emit('Message', task.DbDestination)
+                getConnection(task.DbDestination, config.DataSource, function(err, destConn) {
+                    if (err) {
+                        callback(err)
+                    } else if (destConn == null) {
+                        callback(`Fail to connection to ${task.DbDestination}.`)
+                    } else {
+                        var query = `select * from ${task.TableName}`
+                        emitter.emit('Query', `${task.DbSource}: Execute: ${query}`)
+
+                        srcConn.execute(query, function(error, selectedRows) {
+                            if (error) {
+                                callback(error)
+                            } else if (selectedRows.length <= 0) {
+                                callback(null)
+                            } else {
+                                var columns = []
+                                for (var column in selectedRows[0]) {
+                                    if (selectedRows[0].hasOwnProperty(column)) {
+                                        columns.push(column);
+                                    }
+                                }
+                                var insertColumns = columns.join(',')
+                                var insertedRows = 0
+                                var insertRow = function(idx) {
+                                    if (idx >= selectedRows.length) {
+                                        emitter.emit('Message', `${name} Task: ${idx} rows are copied.`)
+                                        callback(null)
+                                    } else {
+                                        var vals = []
+                                        for(var column of columns) {
+                                            var val = selectedRows[idx][column]
+                                            if (val == null) {
+                                                vals.push('null')
+                                            } else if (isNaN(val)) {
+                                                vals.push(`'${val}'`)
+                                            } else {
+                                                vals.push(val)
+                                            }
+                                        }
+                                        var insertVals = vals.join(',')
+                                        var insert = `insert into ${task.TableName} (${insertColumns}) values(${insertVals})`
+                                        destConn.execute(insert, function(error) {
+                                            if (error) {
+                                                callback(error)
+                                            } else {
+                                                insertRow(++idx)
+                                            }
+                                        })
+                                    }
+                                }
+                                insertRow(0)
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    } else {
+        callback(null)
+    }
+}
+
+function insertData(destConn, rows, name, task, config, emitter, callback) {
+    var Param = config.Param
+
+    var executeQuery = function(rowIdx, Row, queryIdx) {
+        if (queryIdx >= task.DbDestination.Queries.length) {
+            insertRow(++rowIdx)
+        } else {
+            if (Object.keys(Row).length <= 0) {
+                executeQuery(rowIdx, Row, ++queryIdx)
+            } else {
+                var insert = eval("`" + task.DbDestination.Queries[queryIdx] + "`")
+                emitter.emit('Query', `${task.DbDestination.Name}: Execute: ${insert}`)
+
+                destConn.execute(insert, function(error) {
+                    if (error) {
+                        callback(error)
+                    } else {
+                        executeQuery(rowIdx, Row, ++queryIdx)
+                    }
+                })
+            }
+        }
+    }
+
+    var context = {}
+    context['Name'] = name
+    context['Task'] = task
+
+    var insertRow = function(rowIdx) {
+        if (rowIdx >= rows.length) {
+            emitter.emit('Message', `${name} Task: ${rowIdx} rows are Inserted.`)
+            callback(null)
+        } else {
+            var row = rows[rowIdx]
+            var runTransform = function(idx) {
+                if (idx >= task.Transforms.length) {
+                    executeQuery(rowIdx, row, 0)
+                } else {
+                    var transform = task.Transforms[idx]
+                    config.Transforms[transform](
+                        row, context,
+                        function(err) {
+                            if (err) {
+                                callback(err)
+                                return
+                            }
+                            runTransform(++idx)
+                        })
+                }
+            }
+            runTransform(0)
+        }
+    }
+
+    insertRow(0)
+}
+
+function insertDbData(name, task, config, emitter, callback) {
+    emitter.emit('Message', `Start ${task.TaskType} ${name}.`)
+
+    if (task.DbSource.Query != null &&
+        task.DbDestination.Queries != null && util.isArray(task.DbDestination.Queries)) {
+        emitter.emit('Message', task.DbSource.Name)
+
+        getConnection(task.DbSource.Name, config.DataSource, function(err, srcConn) {
+            if (err) {
+                callback(err)
+            } else if (srcConn == null) {
+                callback(`Fail to connection to ${task.DbSource.Name}.`)
+            } else {
+                emitter.emit('Message', task.DbDestination.Name)
+                getConnection(task.DbDestination.Name, config.DataSource, function(err, destConn) {
+                    if (err) {
+                        callback(err)
+                    } else if (destConn == null) {
+                        callback(`Fail to connection to ${task.DbDestination}.`)
+                    } else {
+                        var Param = config.Param
+                        var query = eval('`' + task.DbSource.Query + '`')
+                        emitter.emit('Query', `${task.DbSource.Name}: Execute: ${query}`)
+
+                        srcConn.execute(query, function(error, selectedRows) {
+                            if (error) {
+                                callback(error)
+                            } else if (selectedRows.length <= 0) {
+                                callback(null)
+                            } else {
+                                insertData(destConn, selectedRows, name, task, config, emitter, callback)
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    } else {
+        callback(null)
+    }
+}
+
+function insertDbDataFromCsv(name, task, config, emitter, callback) {
+    emitter.emit('Message', `Start ${task.TaskType} ${name}.`)
+
+    if ( task.CsvSource != null && task.CsvSource.File != null &&
+         task.CsvSource.SkipHeader != null && task.CsvSource.Delimiter != null ) {
+        emitter.emit('Message', task.DbDestination.Name)
+
+        var skipLine = task.CsvSource.SkipHeader
+        getConnection(task.DbDestination.Name, config.DataSource, function(err, destConn) {
+            if (err) {
+                callback(err)
+            } else if (destConn == null) {
+                callback(`Fail to connection to ${task.DbDestination}.`)
+            } else {
+                emitter.emit('Message', task.CsvSource.File)
+                var csvData = []
+                fs.createReadStream(task.CsvSource.File)
+                    .pipe(parse({delimiter: task.CsvSource.Delimiter}))
+                    .on('data', function(csvRow) {
+                        if (skipLine === true) {
+                            skipLine = false
+                        } else {
+                            csvData.push(csvRow)
+                        }
+                    })
+                    .on('end',function() {
+                        insertData(destConn, csvData, name, task, config, emitter, callback)
+                    })
+            }
+        })
+    } else {
+        callback(null)
+    }
+}
+
+function executeTask(name, task, config, emitter, callback) {
+    if (task.TaskType == null) {
+        callback(null)
+    } else if (task.TaskType === 'Run SQL') {
+        runSQL(name, task, config, emitter, callback)
+    } else if (task.TaskType === 'Copy DB Table') {
+        copyDbTable(name, task, config, emitter, callback) 
+    } else if (task.TaskType === 'Insert DB data') {
+        insertDbData(name, task, config, emitter, callback)
+    } else if (task.TaskType === 'Insert CSV data') {
+        insertDbDataFromCsv(name, task, config, emitter, callback)
+    } else {
+        callback(null)
+    }
+}
+
+DataFlowTask.prototype.Start = function start(param, transforms, callback) {
+    callback = getCallback(callback)
+
+    var self = this
+    var config = {}
+    config.DataSource = JSON.parse(JSON.stringify(self.DataSource))
+    config.DataFlow = JSON.parse(JSON.stringify(self.DataFlow))
+    config.Param = JSON.parse(JSON.stringify(param))
+
+    config.Transforms = {}
+    for(var transform of transforms) {
+        config.Transforms[transform.name] = transform
+    }
+
+    var keys = []
+    for (var key in config.DataFlow) {
+        if (config.DataFlow.hasOwnProperty(key)) {
+            keys.push(key)
+        }
+    }
+
+    var iterateTasks = function(idx) {
+        if (idx >= keys.length) {
+            callback(null)
+        } else {
+            var name = keys[idx]
+            var task = config.DataFlow[name]
+            if (util.isArray(task) === true) {
+                var completed = 0
+                for(var i=0; i<task.length; ++i) {
+                    executeTask(
+                        name + `:${i}`, task[i], config, self,
+                        function(err) {
+                            if (err) {
+                                callback(err)
+                                return
+                            }
+                            if (completed >= task.length) {
+                                iterateTasks(++idx)
+                            } else {
+                                ++completed
+                            }
+                        })
+                }
+            } else {
+                executeTask(
+                    name, task, config, self,
+                    function(err) {
+                        if (err) {
+                            callback(err)
+                            return
+                        }
+                        iterateTasks(++idx)
+                    })
+            }
+        }
+    }
+    iterateTasks(0)
+}
+
+module.exports = DataFlowTask
+
+var dataSources = {
+    'MS SQL Source': {
+        DB: 'MSSQL',
+        ConnectionString: "Data Source=localhost\\SQL2014;Initial Catalog=QRSNet;Integrated Security=True"
+    },
+    'MS SQL Dest': {
+        DB: 'MSSQL',
+        ConnectionString: "Data Source=localhost\\SQL2014;Initial Catalog=QRSNet;Integrated Security=True"
+    }
+}
+
+var dataFlow = {
+    Cleanup: {
+		TaskType: 'Run SQL',
+		DbSource: 'MS SQL Source',
+		Queries: [
+            'DELETE FROM fok',
+            "insert into fok(a,b,c,d) values(10,'10',SYSDATETIME(),SYSDATETIME())",
+            "insert into fok(a,b,c,d) values(20,'20',SYSDATETIME(),SYSDATETIME())",
+            "insert into fok(a,b,c,d) values(30,'30',SYSDATETIME(),SYSDATETIME())",
+            "insert into fok(a,b,c,d) values(null,'10',SYSDATETIME(),SYSDATETIME())",
+            "insert into fok(a,b,c,d) values(10,null,SYSDATETIME(),SYSDATETIME())",
+            "insert into fok(a,b,c,d) values(20,'20',null,SYSDATETIME())",
+            "insert into fok(a,b,c,d) values(20,'20',SYSDATETIME(),null)"
+        ],
+	},
+	CopyTable: {
+		TaskType: 'Copy DB Table',
+		DbSource: 'MS SQL Source',
+		DbDestination: 'MS SQL Dest',
+		TableName: 'fok',
+	},
+	InsertData: [
+		{
+			TaskType: 'Insert DB data',
+			DbSource: {
+				Name: 'MS SQL Source',
+				Query: 'Select a as COL1, b as COL2 from fok where a = ${Param.ID}'
+			},
+			Transforms: ['t1', 't3', 't2'],
+			DbDestination: {
+				Name: 'MS SQL Dest',
+				Queries: [
+                    "Insert Into fok (a, b) Values(${Row.COL1}, '${Row.COL2}')"
+                ]
+			}
+		},
+		{
+			TaskType: 'Insert CSV data',
+			CsvSource: {
+				File: './test.csv',
+                SkipHeader: true,
+				Delimiter: ','
+			},
+			Transforms: ['t4'],
+			DbDestination: {
+				Name: 'MS SQL Dest',
+				Queries: [
+                    "Insert Into fok (a,b) Values(${Row[0]},'${Row[1]}')",
+                    "Insert Into fok (b) Values('${Row[2]}')"
+                ]
+            }
+		}
+	]
+}
+
+function t1(row, context, callback) {
+    row.COL1 = 1000
+    console.log('Call t1')
+    console.dir(context)
+    context['Row'] = row
+    callback()
+}
+function t2(row, context, callback) {
+    row.COL2 = ''
+    console.log('Call t2')
+    console.dir(context)
+    callback()
+}
+function t3(row, context, callback) {
+    row.COL2 = 'Test'
+    console.log('Call t3')
+    console.dir(context)
+    callback()
+}
+function t4(row, context, callback) {
+    row[2] = 'Row:' + row[2]
+    console.log('Call t4')
+    console.dir(context)
+    callback()
+}
+
+var dft = DataFlowTask(dataSources, dataFlow)
+dft.on('Message', function(msg) {
+    console.log('Message:', msg)
+})
+dft.on('Query', function(msg) {
+    console.log('Query:', msg)
+})
+dft.Start({ ID: 10 }, [t1, t2, t3, t4],  function(err) {
+    console.log('Error:', err)
+})
